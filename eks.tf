86234122a898bde63f815cb65776083c62628f0a
Ryan Faircloth <ryan@dss-i.com>
2022-05-16 14:28:24 -0400

    feat: add cert manager

--- eks.tf
+++ eks.tf
@@ -134,6 +134,15 @@ provider "kubernetes" {
 }
 
 
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+
 output "cluster_id" {
   value = module.eks.cluster_id
 }
