e30a84003beeef6a71a84eb7245e41bf42cd576c
Ryan Faircloth <ryan@dss-i.com>
2022-05-16 12:14:18 -0400

    feat: deploy vpc

---
+++ main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    aws = {
+      version = ">= 2.28.1"
+    }
+    kubernetes = {
+      version = "~> 2.11"
+    }
+    random = {
+      version = "~> 3.1"
+    }
+    local = {
+      version = "~> 2"
+    }
+  }
+}
+
+provider "aws" {
+  # ... other configuration ...
+  default_tags {
+    tags = {
+      Name          = local.cluster_name
+      Environment   = var.environment
+      Owner         = var.owner
+      App           = "humio"
+      DeployVersion = "0.1.0"
+      ManagedBy     = "Terraform"
+    }
+  }
+}
+data "aws_caller_identity" "current" {}
+data "aws_organizations_organization" "current" {}
+
+
+locals {
+  cluster_name = "${var.deployment_name}-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length  = 4
+  special = false
+  lower   = true
+  upper   = false
+  number  = false
+}
---
+++ vpc.tf
@@ -0,0 +1,31 @@
+data "aws_availability_zones" "available" {
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.14.0"
+
+  #name                 = local.cluster_name
+  cidr                 = "10.0.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["10.0.0.0/20", "10.0.16.0/20", "10.0.32.0/20"]
+  public_subnets       = ["10.0.48.0/20", "10.0.64.0/20", "10.0.80.0/20"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
+}
+
+output "vpc_arn" {
+  value = module.vpc.vpc_arn
+}
+
